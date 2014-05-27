dd9db02bfcc2f5cda8cbb4bd9a7b8ee7cc1a6c5d
Miguel Pinto <mcpinto98@gmail.com>
2014-05-27 21:25:06 +0100

    pipes com nome

--- mainServer.c
+++ mainServer.c
@@ -1,10 +1,32 @@
 #include "struct.h"
 #include "linkedlist/linkedlist.h"
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int main()
-{
+int main() {
+	int mk = mkfifo("/tmp/options",0666);
+
 	LinkedList ll = createLinkedList(NULL,NULL);
+
+	int pp = open("/tmp/options", O_RDONLY, 0700);
+	int n = 0;
+	char buff[1024];
+
+	while(1) {
+		n = read(pp,buff,1024);
+		
+		char c = buff[0];
+		
+	
+
+		write(1,buff,n);
+	}
+
 
 	Distrito d1 = criaDistrito("Braga",3214);
 	Distrito d2 = criaDistrito("Porto",3214);
