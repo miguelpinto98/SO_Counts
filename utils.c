1c524dfc79c9af632342b9f8dc48af8af4c31ddc
serafimpinto <smcp92@gmail.com>
2014-06-03 04:41:59 +0100

    done

--- utils.c
+++ utils.c
@@ -123,7 +123,7 @@ int incrementar(char *nome[], int valor) {
 	if(file < 0) { 
 		file = open(distritoNome, O_WRONLY | O_CREAT, 0777);
 		char* line = malloc(100);
-		sprintf(line,"%s:%s:%d\n",nome[1],nome[2],valor);
+		sprintf(line,"%s:%s:%s:%d\n",nome[0],nome[1],nome[2],valor);
 		write(file,line,strlen(line));
 		free(line);
 		close(file);
@@ -133,23 +133,23 @@ int incrementar(char *nome[], int valor) {
 		//caso concelho-freguesia não exista
 		if(strstr(buffer,nome[1]) == NULL) {
 			char* new = malloc(100);
-			sprintf(new,"%s:%s:%d\n",nome[1],nome[2],valor);
+			sprintf(new,"%s:%s:%s:%d\n",nome[0],nome[1],nome[2],valor);
 			write(file,new,strlen(new));
 			free(new);
 			close(file);
 		}
 		char* line = malloc(xx);
-		sprintf(line,"%s:%s:",nome[1],nome[2]);
+		sprintf(line,"%s:%s:%s:",nome[0],nome[1],nome[2]);
 		if(strstr(buffer,line) == NULL) {
 			char* new = malloc(100);
-			sprintf(new,"%s:%s:%d\n",nome[1],nome[2],valor);
+			sprintf(new,"%s:%s:%s:%d\n",nome[0],nome[1],nome[2],valor);
 			write(file,new,strlen(new));
 			free(new);
 			close(file);
 		} 
 		else {
 			char* line = malloc(xx);
-			sprintf(line,"%s:%s:",nome[1],nome[2]);
+			sprintf(line,"%s:%s:%s:",nome[0],nome[1],nome[2]);
 			inc = devolveValor(buffer,line,&numLidos,&chPorLer);
 			inc += valor;
 
@@ -198,28 +198,162 @@ int incrementar(char *nome[], int valor) {
 	return 1;
 }
 
-int agregar(char* prefixo[], int nivel, char* path) {
-	int file, distrito, i = 0, xx = 0, pos = 0, j = 0;
-	file = open(path, O_WRONLY | O_CREAT, 0777);
+int tamanho(char* prefixo[]) {
+	int i = 0;
+	while(prefixo[i] != NULL) {
+		i++;
+	}
 
+	return i;
+}
+
+int agregar(char* prefixo[], int nivel, char* path) {
+	int size = tamanho(prefixo);
+	if(size == 0) {
+	}
+	if(size == 1) {
+		agregaUm(prefixo,nivel,path);
+	}
+	if(size == 2) {
+		agregaDois(prefixo,nivel,path);
+	}
+	if(size == 3) {
+		agregaTres(prefixo,nivel,path);
+	}
+}
+
+int agregaUm(char* prefixo[], int nivel, char* path) {
+	int file, distrito, xx = 0, i = 0, j = 0;
+	file = open(path, O_WRONLY | O_CREAT, 0777);
 	if(nivel == 0) {
 		char* agregacao = malloc(100);
-		
 		Distrito d = NULL;
 		d = verificaDistrito(ll,prefixo[0]);
 		int total = d->agregado;
-
-		sprintf(agregacao,"TOTAL:%d\n",total);
+		sprintf(agregacao,"%s:%d\n",prefixo[0],2);
 		write(file,agregacao,strlen(agregacao));
 		close(file);
 		free(agregacao);
 	}
-	if (nivel == 1) {
+	if(nivel == 1) {
+		char* visitados = malloc(100);
+		distrito = open(prefixo[0],O_RDWR,0700);
+		char* buffer = malloc(1024);
+		xx = read(distrito,buffer,1024);
+		i = 0;
+		close(distrito);
+		int n = contaN(buffer);
+		while(i < n) {
+			char* pointer = strstr(buffer,prefixo[0]);
+			int inc = strcspn(pointer,"\n");
+			pointer = pointer + strlen(prefixo[0]) + 1;
+	
+			int indice = strcspn(pointer,":");
+			char* auxCon = malloc(100);
+			strncpy(auxCon,pointer,indice);
+			if( (strstr(visitados,auxCon)) == NULL) {
+				strcat(visitados,auxCon);
+				printf("%s\n",auxCon);
+				agregacaoConcelho(auxCon,prefixo[0],file);
+			}
+			buffer = buffer + inc;
+			i++;
+		}
+	}
+	if(nivel == 2) {
+		distrito = open(prefixo[0],O_RDWR,0700);
+		char* buffer = malloc(1024);
+		xx = read(distrito,buffer,1024);
+		close(distrito);
+		write(file,buffer,xx);
+		close(file);
+		free(buffer);
+	}
+	return 1;
+}
+
+int agregacaoConcelho(char* concelho, char* dis, int file) {
+//Distrito:Concelho:200
+	int xx = 0, i = 0, j = 0;
+	int distrito = open(dis,O_RDWR,0700);
+	char* buffer = malloc(1024);
+	char* bufferAux = malloc(1024);
+	int total=0;
+	xx = read(distrito,buffer,1024);
+	close(distrito);
+	char* nome = malloc(200);
+	char* tot = malloc(10);
+	sprintf(nome,"%s:%s:",dis,concelho);
+	int n=contaN(buffer);
+	while((strstr(buffer,nome))!= NULL){
+		char* pointer = strstr(buffer,nome);
+		int ind0 = strcspn(pointer,"\n");
+		pointer=pointer+strlen(dis)+strlen(concelho)+2;
+		int ind1 = strcspn(pointer,":");
+		pointer=pointer+ind1+1;
+		char* valor = malloc(10);
+		int ind2 = strcspn(pointer,"\n");
+		strncpy(valor,pointer,ind2);
+		int val = atoi(valor);
+		total = total + val;
+		sprintf(tot,"%d\n",total);
+			while(buffer[i]!='\n'){
+				i++;
+			}
+			buffer = buffer+i+2;
+	}
+	strcat(nome,tot);
+	write(file,nome,strlen(nome));
+
+	return 1;
+}
+
+int agregaDois(char* prefixo[], int nivel, char* path) {
+	int file, distrito, xx = 0, i = 0, j = 0;
+	file = open(path, O_WRONLY | O_CREAT, 0777);
+	
+	if(nivel == 0) {
+		//Distrito:Concelho:200
+		distrito = open(prefixo[0],O_RDWR,0700);
+		char* buffer = malloc(1024);
+		char* bufferAux = malloc(1024);
+		int total=0;
+		xx = read(distrito,buffer,1024);
+		close(distrito);
+		char* nome = malloc(200);
+		char* tot = malloc(10);
+		sprintf(nome,"%s:%s:",prefixo[0],prefixo[1]);
+		int n=contaN(buffer);
+		while((strstr(buffer,nome))!= NULL){
+			char* pointer = strstr(buffer,nome);
+			int ind0 = strcspn(pointer,"\n");
+			pointer=pointer+strlen(prefixo[0])+strlen(prefixo[1])+2;
+			int ind1 = strcspn(pointer,":");
+			pointer=pointer+ind1+1;
+			char* valor = malloc(10);
+			int ind2 = strcspn(pointer,"\n");
+			strncpy(valor,pointer,ind2);
+			int val = atoi(valor);
+			total = total + val;
+			sprintf(tot,"%d\n",total);
+				while(buffer[i]!='\n'){
+					i++;
+				}
+				buffer = buffer+i+2;
+		}
+		strcat(nome,tot);
+		write(file,nome,strlen(nome));
+
+	}
+
+	if(nivel == 1) {
+		//Distrito:Concelho:Freguesias:Valor
 		distrito = open(prefixo[0],O_RDWR,0700);
 		char* buffer = malloc(1024);
 		char* bufferAux = malloc(1024);
 		xx = read(distrito,buffer,1024);
 		close(distrito);
+		int pos;
 		char* concelho = prefixo[1];
 		while(j < contaN(buffer)) {
 			char* line = malloc(100);
@@ -236,59 +370,37 @@ int agregar(char* prefixo[], int nivel, char* path) {
 				strcat(bufferAux,line);
 			}
 			j++;
-		}
-		//filtrar concelho
-		j=0;
-		while(bufferAux[j] != '\0') {
-			int indice = strcspn(bufferAux,":");
-			char* aux = malloc(1024);
-			char* aux2 = malloc(100);
-			strcpy(aux,bufferAux+indice+1+j);
-			i = 0;
-			while(aux[i] != '\n') {
-				i++;
-			}
-			strncpy(aux2,bufferAux+indice+j+1,i+1);
-			write(file,aux2,strlen(aux2));
-			j = j + strlen(aux2) + strlen(concelho) + 1;
+			free(line);
 		}
 
-		int total = somatorio(bufferAux);
-		char* agregacao = malloc(100);
-		sprintf(agregacao,"\nTOTAL:%d\n",total);
-		write(file,agregacao,strlen(agregacao));
+		write(file,bufferAux,strlen(bufferAux));
 		close(file);
 		free(buffer);
 		free(bufferAux);
-		free(agregacao);
 	}
+	return 1;
+}
 
-	if(nivel == 2) {
+int agregaTres(char* prefixo[], int nivel, char* path) {
+	int file, distrito, xx = 0, i = 0, j = 0;
+	file = open(path, O_WRONLY | O_CREAT, 0777);
+	
+	if(nivel == 0) {
+		//Distrito:Concelho:Freguesia:Valor
 		distrito = open(prefixo[0],O_RDWR,0700);
 		char* buffer = malloc(1024);
 		xx = read(distrito,buffer,1024);
 		close(distrito);
-		char* freguesia = prefixo[2];
-
-		char* pointer = strstr(buffer,freguesia);
-		char* aux = malloc(100);
-		i = 0;
-		while(pointer[i] != '\n') {
-			i++;
-		}
-		strncpy(aux,pointer,i+1);
-		int total = somatorio(aux);
-		free(aux);
-		sprintf(aux,"%d\n",total);
-		write(file,aux,strlen(aux));
-
-		char* agregacao = malloc(100);
-		sprintf(agregacao,"\nTOTAL:%d\n",total);
-		write(file,agregacao,strlen(agregacao));
+		char* nome = malloc(200);
+		sprintf(nome,"%s:%s:%s:",prefixo[0],prefixo[1],prefixo[2]);
+		char* pointer = strstr(buffer,nome);
+		int ind = strcspn(pointer,"\n");
+		strncpy(nome,pointer,ind+1);
+		write(file,nome,strlen(nome));
 		close(file);
 		free(buffer);
-		free(agregacao);
-	}
+		free(nome);
+		}
 	return 1;
 }
 
@@ -322,39 +434,3 @@ void preparaPedido(char *buffer, LinkedList list) {
 	}
 }
 
-/*
-int main() {
-	char* array[3];
-	array[0]="Porto";
-	array[1]="Felgueiras";
-	array[2]="Revinhade";
-	incrementar(array,1);
-
-	char* array2[3];
-	array2[0]="Porto";
-	array2[1]="Porto";
-	array2[2]="Lordelo do Ouro";	
-	incrementar(array2,2);
-
-	array[0]="Porto";
-	array[1]="Felgueiras";
-	array[2]="Revinhade";
-	incrementar(array,100);
-
-	preparaPedido("i:5:Porto:Felgueiras:Revinhade",31);
-	preparaPedido("i:10:Porto:Porto:Lordelo do Ouro",33);
-	preparaPedido("i:5:Porto:Felgueiras:Revinhade",31);
-
-	preparaPedido("a:0:Porto:ADA",14);
-
-	char* agrega[2];
-	agrega[0] = "Porto";
-	agrega[1] = "Felgueiras";
-	agrega[2] = "Revinhade";
-	//agregar(agrega,2,"Agrega_Revinhade");
-	char* buffer = "Felgueiras:Revinhade:30\nFelgueiras:Torrados:20\nFelgueiras:Sousa:10\n";
-	char* search = "Felgueiras:Torrados:";
-	//printf("%d\n",devolveValor(buffer,search));
-	//somatorio(buffer);
-	return 1;
-}*/
